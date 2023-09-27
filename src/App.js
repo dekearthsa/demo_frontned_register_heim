6504c646a2bd20f398ea6e39a2e2848aa371cddd
pcsishun <pcsishun@gmail.com>
2023-09-27 16:15:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import PageRegister from './view/PageRegister.js';
+
+
+// const haddleRegister = async () => {
+//     const status = await axios.post("http://localhost:9292/api/line/regsiter");
+// }
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path="/reigster/:userid" element={< PageRegister/>}></Route>
+        </Routes>
       </header>
     </div>
   );
---
+++ src/view/PageRegister.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+
+
+const PageRegister = () => {
+    const {userid} = useParams();
+
+    const [selectPlant, setSelectPlant] = useState("Rangsit");
+    const [userId, setUserId] = useState(userid)
+
+
+    const haddleRegister = async () => {
+        // console.log(selectPlant,userId)
+        const date = new Date();
+        const ms = date.getTime();
+        const payload = {
+            plantName: selectPlant,
+            lineUserId: userId,
+            getMilisec: ms
+        }
+        const status = await axios.post("http://localhost:9292/api/line/register",payload);
+        if(status.true){
+            alert(status.desc)
+        }else{
+            alert("Can't save please try agian.")
+        }
+    }
+
+
+    return (
+        <>
+            <div className="container-register">
+                <div className="container-id">
+                    <label>Line Id</label>
+                    <input className="container-input-id"  
+                    onChange={(evt) => {setUserId(evt.target.value)}}
+                    value={userId} disabled/>
+                </div>
+                <div className="container-plant">
+                    <label htmlFor="plant">Plant Name</label>
+                    <select className="container-input-plant" 
+                    onChange={(evt) => {setSelectPlant(evt.target.value)}}
+                    value={selectPlant} 
+                    name="plant" id="plant">
+                        <option value="Rangsit">Rangsit</option>
+                        <option value="Bangsue">Bangsue</option>
+                    </select>
+
+                    {/* <input className="container-input-plant" /> */}
+                </div>
+                <div className="container-btn-reg">
+                    <button className="btn-reg" onClick={haddleRegister}>
+                        REGISTER
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PageRegister
+
